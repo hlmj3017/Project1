73183d376b197bddf1c5a1b7c0c8ae91adfa3f71
hlmj3 <hlmj3017@naver.com>
2023-08-17 15:39:22 +0900

    commit

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ def add_func(n1,n2):
 def sub_func(n1, n2) :
 return n1-n2
 
+def mul_func(n1, n2) :
+return n1*n2
+
+def div_func(n1, n2) :
+return n1/n2
+
+def power_func(n1, n2) :
+return n1**n2
+
 ## 전역 변수부
 num1, num2 = 100, 200
 result = 0
@@ -15,3 +24,12 @@ print(num1, "+", num2, "=", result)
 
 result = sub_func(num1, num2)
 print(num1, "-", num2, "=", result)
+
+result = mul_func(num1, num2)
+print(num1, "*", num2, "=", result)
+
+result = div_func(num1, num2)
+print(num1, "/", num2, "=", result)
+
+result = power_func(num1, num2)
+print(num1, "**", num2, "=", result)
